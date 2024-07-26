bfa1ba366b64e624e7f2bae6b089a9dfb8848ead
LeandroMassola <leandromassola.lm@gmail.com>
2024-07-26 18:07:32 +0200

    creé about y contact, falta hacer formulario con validaciones

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,5 +1,13 @@
 module.exports = {
     getIndex: (req, res) => {
         res.render('main/home');
+    },
+
+    getAbout: (req, res) => {
+        res.render('main/about')
+    },
+
+    getContact: (req, res) => {
+        res.render('main/contact')
     }
 }
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const mainController = require('../controllers/mainController');
 
 router.get('/', mainController.getIndex);
+router.get('/about', mainController.getAbout)
+router.get('/contact', mainController.getContact)
+
 
 module.exports = router;
