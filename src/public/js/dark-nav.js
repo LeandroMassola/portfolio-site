6f9fd4e0eaec3487bed80da25a5502760e9a0a69
LeandroMassola <leandromassola.lm@gmail.com>
2024-08-07 01:16:07 +0200

    añadidas validaciones back end y front end a form de contacto

--- src/public/js/dark-nav.js
+++ src/public/js/dark-nav.js
@@ -3,12 +3,24 @@ let linksNav = document.querySelectorAll('.links__nav')
 let logoNav = document.querySelector('.logo__nav')
 let footer = document.querySelector('.footer')
 
+window.addEventListener('resize', toDarkNav)
 
-if(nav.classList.contains('nav')) {
-    linksNav.forEach(link => {
-        link.classList.add('links-dark__nav')
-    })
-    nav.classList.add('dark__nav')
-    logoNav.classList.add('logo-dark__nav')
-    footer.classList.add('dark__footer')
+
+
+function toDarkNav() {
+    if(window.innerWidth >= 1024) {
+        linksNav.forEach(link => {
+            link.classList.add('links-dark__nav')
+        })
+        nav.classList.add('dark__nav')
+        logoNav.classList.add('logo-dark__nav')
+        footer.classList.add('dark__footer')
+    } else {
+        linksNav.forEach(link => {
+            link.classList.remove('links-dark__nav')
+        })
+        nav.classList.remove('dark__nav')
+        logoNav.classList.remove('logo-dark__nav')
+        footer.classList.remove('dark__footer')
+    }
 }
